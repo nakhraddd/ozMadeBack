c2a3c0ef73692a34d90f02c65020d2e6943780da
darkhan <d_tastanov@kbtu.kz>
2026-02-23 11:33:38 +0500

    conf

--- config/config.go
+++ config/config.go
@@ -1,17 +1,24 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
+	// We ignore the error because in Docker/Production,
+	// variables are often provided by the environment, not a file.
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+		log.Println("Note: Using system environment variables (no .env file found)")
 	}
 }
 
-func GetEnv(key string) string {
-	return os.Getenv(key)
+func GetEnv(key string, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
