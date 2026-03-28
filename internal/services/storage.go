7ec8df4b8ae79e14ab33eafc7d8fa99a364973a6
darkhan <d_tastanov@kbtu.kz>
2026-03-28 13:53:45 +0500

    internal/services/storage.go:12:50: not enough arguments in call to GCS.GenerateSignedURL

--- internal/services/storage.go
+++ internal/services/storage.go
@@ -9,5 +9,5 @@ func GenerateSignedURL(objectName string) (string, error) {
 	if GCS == nil {
 		return "", fmt.Errorf("GCS service not initialized")
 	}
-	return GCS.GenerateSignedURL(objectName, "GET", 15*time.Minute)
+	return GCS.GenerateSignedURL(objectName, "GET", 15*time.Minute, "")
 }
